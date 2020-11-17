94ce7fcd766aaf8600126b2a5ce6ed7cd49e2498
Shivani <cwanikarna@gmail.com>
2020-11-17 20:18:21 +0545

    Font page and Database Connection.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from os import name
 
 from flask import Flask, render_template, request, redirect, session, url_for
 from flask_mysqldb import MySQL
@@ -6,6 +5,7 @@ import MySQLdb
 
 
 app = Flask(__name__)
+app.secret_key="123456"
 
 app.config["MYSQL_HOST"] = "127.0.0.1"
 app.config["MYSQL_USER"] = "root"
@@ -15,25 +15,43 @@ app.config["MYSQL_DB"] = "user"
 db = MySQL(app)
 
 @app.route('/', methods=['GET','POST'])
-def index(roll=None):
+def index():
     if request.method == 'POST':
-        if 'name' in request.form and 'college' in request.form :
-            Name = request.form['name']
-            College = request.form['College']
-
+        if 'name' in request.form and "college" in request.form :
+            name = request.form['name']
+            college = request.form['college']
             cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-            cursor.execute("SELECT * FROM tired WHERE name=%s and college=%s" , (Name, College))
+            cursor.execute("SELECT * FROM tired WHERE name = %s AND College = %s",(name,college))
             info = cursor.fetchone()
-            if info['name'] == Name and info['college'] == College :
 
-               return "login Successful"
-
+            if info is not None:
+               if info['name'] ==name and info["College"]==college:
+                session['loginsuccess'] = True
+                return redirect (url_for('profile'))
             else:
-               return "unsuccessful,Please Register"
+                return redirect(url_for('index'))
 
     return render_template("login.html")
 
+@app.route('/new',methods=['GET','POST'])
+def new_user():
+    if request.method == "POST":
+       if 'name' in request.form and 'college' in request.form:
+          Name = request.form['name']
+          College = request.form['college']
+          cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+          cursor.execute("INSERT INTO tired(name,College) VALUES(%s,%s)",(Name,College))
+          db.connection.commit()
+          return redirect(url_for('index'))
+
+    return render_template("register.html")
+
+
+@app.route('/new/profile')
+def profile():
+   if  session['loginsuccess'] == True:
+       return render_template('profile.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
